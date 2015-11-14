ed702cb9de583f8ee6cc3513417f4d2b5b1c1819
johnkagga <johnkagga@gmail.com>
2015-11-14 05:46:15 +0300

    Checking for network and internet

--- app/src/main/java/com/hanselandpetal/catalog/MainActivity.java
+++ app/src/main/java/com/hanselandpetal/catalog/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hanselandpetal.catalog;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -9,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,18 +52,57 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.action_do_task) {
-			JokamTask task = new JokamTask();
-            //Parallel processing of the tasks.
-            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
-                    "param1", "param 2", "param 3");
+            if (isOnline())
+            {
+                requestData();
+            }
+            else {
+                Toast.makeText(this,"No network Access",Toast.LENGTH_LONG).show();
+            }
 		}
 		return false;
 	}
 
+    /**
+     * Executes the AsyncTask
+     */
+    private void requestData() {
+        JokamTask task = new JokamTask();
+        //Parallel processing of the tasks.
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
+                "param1", "param 2", "param 3");
+    }
+
+    /**
+     * Appendsa message to the textview
+     * @param message
+     */
 	protected void updateDisplay(String message) {
 		output.append(message + "\n");
 	}
 
+    /**
+     * Checking for network access
+     *
+     * @return boolean
+     */
+    protected boolean isOnline()
+    {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting())
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Makes a network request
+     */
     public class JokamTask extends AsyncTask<String,String,String>{
 
         //Before the task
