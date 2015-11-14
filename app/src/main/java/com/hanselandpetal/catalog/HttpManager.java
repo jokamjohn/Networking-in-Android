84ccd8a7c499c70170d14363fd872ee0ff341a82
johnkagga <johnkagga@gmail.com>
2015-11-14 06:34:13 +0300

    Using the UrlConnection

--- app/src/main/java/com/hanselandpetal/catalog/HttpManager.java
+++ app/src/main/java/com/hanselandpetal/catalog/HttpManager.java
@@ -1,12 +1,10 @@
 package com.hanselandpetal.catalog;
 
-import android.net.http.AndroidHttpClient;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by jokamjohn on 11/14/2015.
@@ -20,19 +18,35 @@ public class HttpManager {
      */
     public static String getData(String uri)
     {
-        AndroidHttpClient httpClient = AndroidHttpClient.newInstance("Android Agent");
-        HttpGet request = new HttpGet(uri);
-        HttpResponse response;
+        BufferedReader reader = null;
 
         try {
-            response = httpClient.execute(request);
-            //Getting the contents of the response
-            return EntityUtils.toString(response.getEntity());
+            URL url = new URL(uri);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) !=null)
+            {
+                sb.append(line).append("\n");
+            }
+
+            return sb.toString();
+
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }finally {
-            httpClient.close();
+            if (reader != null)
+            {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
